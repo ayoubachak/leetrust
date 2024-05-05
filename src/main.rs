e884cb2254555f047200bac127f12f4faee4b1f2
Ayoub Achak <ayoub.achak01@gmail.com>
2024-05-05 20:06:02 +0200

    enable more operators

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,21 @@ fn main() {
     let mut chunk = Chunk::new();
 
     // Add a constant and its usage to the chunk
-    let constant_index = chunk.add_constant(1.2);
+    let mut constant_index = chunk.add_constant(1.2);
     chunk.write(OpCode::Constant, 123);  // Opcode to push constant onto the stack
     chunk.write(OpCode::from_usize(constant_index).unwrap(), 123);  // The index of the constant in the array
-    chunk.write(OpCode::Negate, 123);  // The index of the constant in the array
 
+    constant_index = chunk.add_constant(3.4);
+    chunk.write(OpCode::Constant, 123);  // Opcode to push constant onto the stack
+    chunk.write(OpCode::from_usize(constant_index).unwrap(), 123);  // The index of the constant in the array
+    chunk.write(OpCode::Add, 123);  // Opcode to push constant onto the stack
+
+    constant_index = chunk.add_constant(5.6);
+    chunk.write(OpCode::Constant, 123);  // Opcode to push constant onto the stack
+    chunk.write(OpCode::from_usize(constant_index).unwrap(), 123);  // The index of the constant in the array
+    chunk.write(OpCode::Divide, 123);  // Opcode to push constant onto the stack
+
+    chunk.write(OpCode::Negate, 123);  // The index of the constant in the array
     // Add return operation
     chunk.write(OpCode::Return, 123);
 
